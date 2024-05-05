c2eb72cce77c969aa2a4ad900cb1ad83db04d3c8
Dmitrii K <63146525+th3foxx@users.noreply.github.com>
2024-05-05 11:14:40 +0300

    improve

--- RPSGame/Game.cs
+++ RPSGame/Game.cs
@@ -1,65 +1,98 @@
-﻿namespace RPSGame;
-
-public class Game
+namespace RPSGame
 {
-    private int ComputerScore { get; set; }
-    private int PlayerScore { get; set; }
-
-    public void Run()
+    public class Game
     {
-        var render = new GameRender();
-        
-        while (true)
+        private int ComputerScore { get; set; }
+        private int PlayerScore { get; set; }
+        private GameRender render;
+
+        public Game()
         {
-            render.DrawMenu(PlayerScore, ComputerScore);
-            
-            var computerGesture = render.GetGesture(new Random().Next(1, 3));
-            var playerGesture = render.GetGesture(Player.MakeChoice());
-            
-            render.DrawResult(playerGesture, computerGesture);
-            GetResult(playerGesture, computerGesture);
-            Thread.Sleep(3000);
-            
-            if (PlayerScore == 3 && ComputerScore != 3)
+            render = new GameRender();
+        }
+
+        public void Run()
+        {
+            while (true)
             {
-                Console.WriteLine("You've won!");
-                return;
-            }
-            else if (PlayerScore != 3 && ComputerScore == 3)
-            {
-                Console.WriteLine("Computer has won!");
-                return;
-            }
-            else if (PlayerScore == 3 && ComputerScore == 3)
-            {
-                Console.WriteLine("Draw!");
-                return;
+                render.DrawMenu(PlayerScore, ComputerScore);
+
+                var computerGesture = GetRandomGesture();
+                var playerGesture = GetPlayerGesture();
+
+                render.DrawResult(playerGesture, computerGesture);
+                GetResult(playerGesture, computerGesture);
+                Thread.Sleep(3000);
+
+                if (PlayerScore == 3 && ComputerScore != 3)
+                {
+                    Console.WriteLine("You've won!");
+                    break;
+                }
+                else if (PlayerScore != 3 && ComputerScore == 3)
+                {
+                    Console.WriteLine("Computer has won!");
+                    break;
+                }
+                else if (PlayerScore == 3 && ComputerScore == 3)
+                {
+                    Console.WriteLine("Draw!");
+                    break;
+                }
+
+                Console.WriteLine("Do you want to play again? (yes/no)");
+                if (Console.ReadLine().ToLower() != "yes")
+                {
+                    break;
+                }
+
+                PlayerScore = 0;
+                ComputerScore = 0;
             }
         }
-        
-    }
 
-    private void GetResult(Gestures playerGesture, Gestures computerGesture)
-    {
-        switch (playerGesture)
+        private Gestures GetRandomGesture()
         {
-            case Gestures.Rock when computerGesture == Gestures.Scissors:
-            case Gestures.Paper when computerGesture == Gestures.Rock:
-            case Gestures.Scissors when computerGesture == Gestures.Paper:
-                Console.WriteLine("You've won the round!");
-                PlayerScore++;
-                break;
-            case Gestures.Scissors when computerGesture == Gestures.Rock:
-            case Gestures.Rock when computerGesture == Gestures.Paper:
-            case Gestures.Paper when computerGesture == Gestures.Scissors:
-                Console.WriteLine("Computer has won the round!");
-                ComputerScore++;
-                break;
-            default:
-                Console.WriteLine("Draw!");
-                PlayerScore++;
-                ComputerScore++;
-                break;
+            var random = new Random();
+            var gesture = random.Next(1, 4);
+            return (Gestures)gesture;
+        }
+
+        private Gestures GetPlayerGesture()
+        {
+            Console.WriteLine("Enter your gesture (1: Rock, 2: Paper, 3: Scissors):");
+            if (int.TryParse(Console.ReadLine(), out int gesture))
+            {
+                if (gesture >= 1 && gesture <= 3)
+                {
+                    return (Gestures)gesture;
+                }
+            }
+
+            Console.WriteLine("Invalid gesture input.");
+            return Gestures.None;
+        }
+
+        private void GetResult(Gestures playerGesture, Gestures computerGesture)
+        {
+            switch (playerGesture)
+            {
+                case Gestures.Rock when computerGesture == Gestures.Scissors:
+                case Gestures.Paper when computerGesture == Gestures.Rock:
+                case Gestures.Scissors when computerGesture == Gestures.Paper:
+                    Console.WriteLine("You've won the round!");
+                    PlayerScore++;
+                    break;
+                case Gestures.Scissors when computerGesture == Gestures.Rock:
+                case Gestures.Rock when computerGesture == Gestures.Paper:
+                case Gestures.Paper when computerGesture == Gestures.Scissors:
+                    Console.WriteLine("Computer has won the round!");
+                    ComputerScore++;
+                    break;
+                default:
+                    Console.WriteLine("Draw!");
+                    break;
+            }
         }
     }
 }
