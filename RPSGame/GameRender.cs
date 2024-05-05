2719023a331a7bc7ce2c1f904efe776bd7d91cf6
Dmitrii K <63146525+th3foxx@users.noreply.github.com>
2024-05-05 11:14:42 +0300

    improve

--- RPSGame/GameRender.cs
+++ RPSGame/GameRender.cs
@@ -1,63 +1,138 @@
-﻿namespace RPSGame;
+using System;
 
-public class GameRender
+namespace RPSGame
 {
-    private readonly string[] _gestureArts = new string[3] {@"    _______
+    public class GameRender
+    {
+        private readonly string[] _gestureArts = new string[3]
+        {
+            @"    _______
 ---'   ____)
       (_____)
       (_____)
       (____)
----.__(___)", @"     _______
+---.__(___)",
+            @"     _______
 ---'    ____)____
            ______)
           _______)
          _______)
----.__________)", @"    _______
+---.__________)",
+            @"    _______
 ---'   ____)____
           ______)
        __________)
       (____)
----.__(___)"};
-    
-    private void DrawGesture(Gestures gesture)
-    {
-        switch (gesture)
+---.__(___)"
+        };
+
+        private void DrawGesture(Gestures gesture)
         {
-            case Gestures.Rock:
-                Console.WriteLine(_gestureArts[0]);
-                break;
-            case Gestures.Paper:
-                Console.WriteLine(_gestureArts[1]);
-                break;
-            case Gestures.Scissors:
-                Console.WriteLine(_gestureArts[2]);
-                break;
+            switch (gesture)
+            {
+                case Gestures.ROCK:
+                    Console.WriteLine(_gestureArts[0]);
+                    break;
+                case Gestures.SCISSORS:
+                    Console.WriteLine(_gestureArts[1]);
+                    break;
+                case Gestures.PAPER:
+                    Console.WriteLine(_gestureArts[2]);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(gesture));
+            }
+        }
+
+        public void DrawMenu(int playerScore, int computerScore)
+        {
+            Console.Clear();
+            Console.WriteLine($"Player score: {playerScore} | Computer score: {computerScore}");
+            Console.WriteLine("****************************");
+            Console.WriteLine("*The gesture selection menu*");
+            Console.WriteLine("****************************");
+            Console.WriteLine("*1 - Rock                  *");
+            Console.WriteLine("*2 - Scissors              *");
+            Console.WriteLine("*3 - Paper                 *");
+            Console.WriteLine("****************************");
+            Console.Write("Pick a gesture: ");
+        }
+
+        public void DrawResult(Gestures playerGesture, Gestures computerGesture)
+        {
+            Console.Clear();
+            Console.WriteLine("Player: ");
+            DrawGesture(playerGesture);
+            Console.WriteLine("Computer: ");
+            DrawGesture(computerGesture);
+            DrawWinner(GetWinner(playerGesture, computerGesture));
+        }
+
+        public void DrawWinner(Winners winner)
+        {
+            switch (winner)
+            {
+                case Winners.PLAYER:
+                    Console.WriteLine("You win this round!");
+                    break;
+                case Winners.COMPUTER:
+                    Console.WriteLine("Computer wins this round!");
+                    break;
+                case Winners.DRAW:
+                    Console.WriteLine("This round is a draw!");
+                    break;
+            }
+        }
+
+        public Gestures GetGesture(int gesture) => gesture switch
+        {
+            1 => Gestures.ROCK,
+            2 => Gestures.SCISSORS,
+            3 => Gestures.PAPER,
+            _ => throw new ArgumentOutOfRangeException(nameof(gesture))
+        };
+
+        public Gestures GetComputerGesture() =>
+            (Gestures)new Random().Next(3) + 1;
+
+        public Winners GetWinner(Gestures playerGesture, Gestures computerGesture)
+        {
+            if (playerGesture == computerGesture)
+            {
+                return Winners.DRAW;
+            }
+
+            switch (playerGesture)
+            {
+                case Gestures.ROCK:
+                    return computerGesture == Gestures.SCISSORS
+                        ? Winners.PLAYER
+                        : Winners.COMPUTER;
+                case Gestures.PAPER:
+                    return computerGesture == Gestures.ROCK
+                        ? Winners.PLAYER
+                        : Winners.COMPUTER;
+                case Gestures.SCISSORS:
+                    return computerGesture == Gestures.PAPER
+                        ? Winners.PLAYER
+                        : Winners.COMPUTER;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 
-    public void DrawMenu(int playerScore, int computerScore)
+    public enum Gestures
     {
-        Console.Clear();
-        Console.WriteLine($"Player score: {playerScore} | Computer score: {computerScore}");
-        Console.WriteLine("****************************");
-        Console.WriteLine("*The gesture selection menu*");
-        Console.WriteLine("****************************");
-        Console.WriteLine("*1 - Rock                  *");
-        Console.WriteLine("*2 - Scissors              *");
-        Console.WriteLine("*3 - Paper                 *");
-        Console.WriteLine("****************************");
-        Console.Write("Pick a gesture: ");
+        ROCK,
+        PAPER,
+        SCISSORS
     }
 
-    public void DrawResult(Gestures playerGesture, Gestures computerGesture)
+    public enum Winners
     {
-        Console.Clear();
-        Console.WriteLine("Player: ");
-        DrawGesture(playerGesture);
-        Console.WriteLine("Computer: ");
-        DrawGesture(computerGesture);
+        PLAYER,
+        COMPUTER,
+        DRAW
     }
-    
-    public Gestures GetGesture(int gesture) =>
-        gesture switch { 1 => Gestures.Rock, 2 => Gestures.Scissors, 3 => Gestures.Paper };
 }
